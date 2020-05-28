efd9dde18a8b50d51815692c8bef0a55dc6a2549
Song-In-Love <ybzmemry@gmail.com>
2020-05-28 13:00:44 +0900

    cpp test

---
+++ test.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <stdio.h>
+
+class Test
+{
+private:
+    /* data */
+public:
+    Test(/* args */);
+    ~Test();
+};
+
+Test::Test(/* args */)
+{
+}
+
+Test::~Test()
+{
+}
+
+
+Test* test = new Test();
+
+int main()
+{ 
+    return 0;
+}
